7d6a8a9e258e133feecca988b392e82b8c02cd61
Joe Janicki <joejanicki26@gmail.com>
2025-02-15 16:16:19 +0100

    Show fps/tps in release

--- render.py
+++ render.py
@@ -221,14 +221,14 @@ class Renderer:
 
         self.disp.blit(self.gui.update(self.mouse, self.keys_down), (0, 0))
 
-        if version.DEBUG:
-            self.fps_update += 1
-            if self.fps_update > 60:
-                self.fps_update = 0
-                self.fps_text = f"{round(game.fps,2)}fps, {game.tps}tps"
-            self.disp.blit(
-                self.text.render(self.fps_text), (8, 16)
-            )
+        # if version.DEBUG:
+        self.fps_update += 1
+        if self.fps_update > 60:
+            self.fps_update = 0
+            self.fps_text = f"{round(game.fps,2)}fps, {game.tps}tps"
+        self.disp.blit(
+            self.text.render(self.fps_text), (8, 16)
+        )
 
         self.window.blit(
             pg.transform.scale(
